bad7d7f121a4501f4061f746b69701ea018d930e
pochimi reddy harshitha <pochimi.harshitha@brillio.com>
2022-12-02 17:46:34 +0530

    initial

---
+++ spring_git1/src/main/java/p1/SpringGit1Application.java
@@ -0,0 +1,13 @@
+package p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGit1Application.class, args);
+	}
+
+}
